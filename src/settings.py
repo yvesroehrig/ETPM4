dd921adb67f2d1774cf465522b825b907dfda3d8
Pavel <80854516+brudi16@users.noreply.github.com>
2022-04-19 15:24:13 +0200

    bugfix

---
+++ src/settings.py
@@ -0,0 +1,5 @@
+# global parameters
+DEBUG       = True  # Debug on/of
+Fs          = 14000 # Sampling frequency
+N_Samp      = 512   # Number of Samples
+Filt_ord    = 10    # Order of the digital filter
