9b5c3d2185ce68f0611978d8335bfe6139903e76
Pavel <80854516+brudi16@users.noreply.github.com>
2022-04-20 21:52:12 +0200

    webpage actualized

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,9 @@
 # global parameters
-DEBUG       = True  # Debug on/of
+from wsgiref.simple_server import demo_app
+
+
+DEBUG       = True  # Debug on/off
+DEMO        = True  # Demo on/off
 Fs          = 14000 # Sampling frequency
 N_Samp      = 512   # Number of Samples
 Filt_ord    = 10    # Order of the digital filter
