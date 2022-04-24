44fe4d5efb5fcbc7d7ab0432d31659c9844e6616
Pavel <80854516+brudi16@users.noreply.github.com>
2022-04-25 00:06:45 +0200

    Update adc.c

--- test/adc.c
+++ test/adc.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <i2c/smbus.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <bc2835.h>
+#include <signal.h>
 
 #define MASK_12_BIT 0x0FFF
 #define ADC1 0x08
@@ -14,10 +17,11 @@
 #define CH0 0x80
 #define CH1 0xC0
 #define NUM_PT 1024
+#define BROADCAST 0x6B
 
 int main(void){
   // variables
-  int file_i2c_ADC1, file_i2c_ADC2, length = 2;
+  int file_i2c_ADC1, file_i2c_ADC2, file_i2c_BRDC, length = 2;
   uint16_t data_ADC1[NUM_PT] = {0};
   uint16_t data_ADC2[NUM_PT] = {0};
   uint16_t data [2] = {0};
@@ -35,6 +39,9 @@ int main(void){
     printf("Failed to open the i2c bus\n");
   }
 
+  if((file_i2c_BRDC = open(filename, O_RDWR))<0){
+    printf("Failed to open the i2c bus\n");
+  }
 
   if(ioctl(file_i2c_ADC1, I2C_SLAVE, ADC1) < 0){
     printf("Failed to acquire bus accss and/or talk to ADC1.\n");
@@ -44,34 +51,43 @@ int main(void){
     printf("Failed to acquire bus accss and/or talk to ADC2.\n");
   }
   
+  if(ioctl(file_i2c_BRDC, I2C_SLAVE, BROADCAST) < 0){
+    printf("Failed to acquire bus accss and/or talk to ADC2.\n");
+  }
+
   // write config
   data [0] = CH0;
   if((write(file_i2c_ADC1, data, 1) != 1) || (write(file_i2c_ADC2, data, 1) != 1)){
   printf("Failed to write to the i2c Bus.\n");
   }
 
-  // send test broadcast
-
-  // read bytes
-  gettimeofday(&t1, NULL);
-  for(int i=0;i<NUM_PT;i++){
-    if(read(file_i2c_ADC1, data_ADC1 + i, length) != length){
-      printf("Failed to read from the ADC1.\n");
+  while(1){
+    // read bytes
+    gettimeofday(&t1, NULL);
+    for(int i=0;i<NUM_PT;i++){
+      //(write(file_i2c_BRDC, data, 1));
+      if(i2c_smbus_write_quick(file_i2c_BRDC,1)){
+        printf("Broadcast fehlgeschlagen.\n");
+      }
+      //close(file_i2c_BRDC);
+      if(read(file_i2c_ADC1, data_ADC1 + i, length) != length){
+        printf("Failed to read from the ADC1.\n");
+      }
+      if(read(file_i2c_ADC2, data_ADC2 + i, length) != length){
+        //printf("Failed to read from the ADC2.\n");
+      }
     }
-    if(read(file_i2c_ADC2, data_ADC2 + i, length) != length){
-      //printf("Failed to read from the ADC2.\n");
-    }
-  }
-  
-  gettimeofday(&t2, NULL);
-  elapsedTime = ((t2.tv_sec * 1000000) + t2.tv_usec)- ((t1.tv_sec * 1000000) + t1.tv_usec);
-  printf("Elapsed Time: %lld\n",elapsedTime);
+    
+    gettimeofday(&t2, NULL);
+    elapsedTime = ((t2.tv_sec * 1000000) + t2.tv_usec)- ((t1.tv_sec * 1000000) + t1.tv_usec);
+    printf("Elapsed Time: %lld\n",elapsedTime);
 
-  // print data
-  for(int i=0;i<16;i++){
-    data_ADC1[i] = (data_ADC1[i]>>4) & MASK_12_BIT;
-    data_ADC2[i] = (data_ADC2[i]>>4) & MASK_12_BIT;
-    printf("Value %u ADC1: %u ADC2: %u\n",i+1,data_ADC1[i],data_ADC2[i]);
+    // print data
+    for(int i=0;i<16;i++){
+      data_ADC1[i] = (data_ADC1[i]>>4) & MASK_12_BIT;
+      data_ADC2[i] = (data_ADC2[i]>>4) & MASK_12_BIT;
+      printf("Value %u ADC1: %u ADC2: %u\n",i+1,data_ADC1[i],data_ADC2[i]);
+    }
+    printf("No Error\n END OF FILE!\n");
   }
-  printf("No Error\n END OF FILE!\n");
 }
