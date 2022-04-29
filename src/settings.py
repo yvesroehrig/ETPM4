bf7ea194722286b94762d24f0677f1af29f08925
Pavel Müller <80854516+brudi16@users.noreply.github.com>
2022-04-29 10:36:05 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,8 @@ from wsgiref.simple_server import demo_app
 
 DEBUG       = True  # Debug on/off
 DEMO        = True  # Demo on/off
-Fs          = 14000 # Sampling frequency
+Fs          = 9000 # Sampling frequency
 N_Samp      = 1024   # Number of Samples
 Filt_ord    = 10    # Order of the digital filter
+f_band_low  = 400  # Lower frequency for bandpass
+f_band_high = FS/2 # Higher frequency for bandpass at half 
