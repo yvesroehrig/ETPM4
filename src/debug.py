b9a006438b6ebe67728accddbea68d7f02b64a00
Pavel <80854516+brudi16@users.noreply.github.com>
2022-05-13 21:58:41 +0200

    Update calculations.py

--- src/debug.py
+++ src/debug.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import settings
 
-def plot(sig1,sig2,x_vec,sigName,xLabel,yLabel,title):
+def plot(sig1,sig2,sigName,xLabel,yLabel,title,x_vec=np.linspace(0,settings.N_Samp-1,1)):
     plt.figure(title)
     plt.clf
     plt.plot()
