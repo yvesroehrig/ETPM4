f57409935f6df8e521bff31463229935bb2c2b81
Pavel <80854516+brudi16@users.noreply.github.com>
2022-05-30 11:25:35 +0200

    Update app.py

--- webTest/app.py
+++ webTest/app.py
@@ -2,10 +2,27 @@
 
 import asyncio
 import websockets
+import json
+
+from connect4 import PLAYER1,PLAYER2
 
 async def handler(websocket):
-    async for message in websocket:
-        print(message)
+    for player,column,row in[
+        (PLAYER1, 3, 0),
+        (PLAYER2, 3, 1),
+        (PLAYER1, 4, 0),
+        (PLAYER2, 4, 1),
+        (PLAYER1, 2, 0),
+        (PLAYER2, 1, 0),
+        (PLAYER1, 5, 0),
+    ]:
+        event = {
+            "type": "play",
+            "player": player,
+            "column" : column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
 
 async def main():
     async with websockets.serve(handler, "", 8001):
